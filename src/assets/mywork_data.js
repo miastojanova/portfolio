9e8d1651ea10f9a6ad86ec53dfb069419ff3c1a6
Mia Stojanova <miaastojanova@gmail.com>
2025-10-23 00:36:40 +0200

    changed work

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,16 +1,11 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from '../assets/project_1.PNG'
 
 const mywork_data = [
-    //  {
-    //      w_no:1,
-    //      w_name:"Yet to come",
-    //      w_img:project1_img
-    //  },
+     {
+         w_no:1,
+         w_name:"Personal Portfolio",
+         w_img:project1_img
+     },
     // {
     //     w_no:2,
     //     w_name:"Web design",
